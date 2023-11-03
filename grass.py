0b7ec8ee0e4d9e31dedd7c749da3c15d19b5928c
sujissi_Laptop <hwangsuji329@gmail.com>
2023-11-04 01:45:01 +0900

    이미지 경로 변경

--- grass.py
+++ grass.py
@@ -3,7 +3,7 @@ from pico2d import load_image
 
 class Grass:
     def __init__(self):
-        self.image = load_image('grass.png')
+        self.image = load_image('img.png')
 
     def draw(self):
         self.image.draw(400, 30)
