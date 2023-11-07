9c391eaacafb8655485ebfb2cca4cd60036cc388
sujissi_Laptop <hwangsuji329@gmail.com>
2023-11-07 23:47:03 +0900

    시간 틀 추가

--- horse.py
+++ horse.py
@@ -27,6 +27,17 @@ def space_down(e):
 def time_out(e):
     return e[0] == 'TIME_OUT'
 
+# Run Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
 
 ################################################
 
